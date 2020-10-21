2f4006f7a1a195945e2ee30ec03dc504a61182d2
Tahiya Sultana Safa <merida_s@yahoo.com>
2020-10-21 21:05:07 +0600

    Firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,17 @@ import 'package:login/login.dart';
 import 'package:login/registration.dart';
 import 'package:login/welcome.dart';
 import 'helper.dart';
+import 'package:flutter/material.dart';
+import 'helper.dart';
+import 'auth_helper.dart';
 
-void main() {
+import 'home_pg.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+/*void main() {
+
+
   runApp(Homepage());
 }
 
@@ -13,12 +22,56 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: WelcomeScreen.id,
-      routes:{
-        WelcomeScreen.id:(context)=>WelcomeScreen(),
-        LoginScreen.id:(context)=>LoginScreen(),
-        RegistrationScreen.id:(context)=>RegistrationScreen(),
-      }
+      home: LoginScreen(),
+
+        initialRoute: WelcomeScreen.id,
+        routes:{
+          WelcomeScreen.id:(context)=>WelcomeScreen(),
+          LoginScreen.id:(context)=>LoginScreen(),
+          RegistrationScreen.id:(context)=>RegistrationScreen(),
+
+
+
     );
+  }
+}}*/
+void main() async  {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User>(
+
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder:(context, snapshot) {
+          if(snapshot.hasData && snapshot.data != null){
+            return HomePage();
+          }
+          return LoginScreen();
+        }
+    );
+
+
+
+
   }
 }
